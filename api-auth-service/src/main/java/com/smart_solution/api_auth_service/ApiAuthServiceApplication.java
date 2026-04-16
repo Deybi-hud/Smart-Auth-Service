193649b0fcd7ad9a5c19c6222ff5357040493e00
deybi-hud <hu4nt4r@gmail.com>
2026-04-16 16:12:25 -0400

    Inicio

---
+++ api-auth-service/src/main/java/com/smart_solution/api_auth_service/ApiAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smart_solution.api_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthServiceApplication.class, args);
+	}
+
+}
---
+++ api-auth-service/src/test/java/com/smart_solution/api_auth_service/ApiAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_solution.api_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
